3ed43dbc104ad582ae84dd43647b9be6cbb0b3a4
Artem Dyakovich <117192385+dyakart@users.noreply.github.com>
2025-06-19 08:46:50 +0300

    Update setup_memcached.sh

--- scripts/setup_memcached.sh
+++ scripts/setup_memcached.sh
@@ -7,13 +7,11 @@ ASTRA="192.168.0.22"
 # 1. Устанавливаем пакет
 run "$ALT" "command -v memcached || (apt-get -yq install memcached || yum -y install memcached)"
 
-# 2. Конфиг: меняем слушающий адрес
-run "$ALT" "if [ -f /etc/memcached.conf ]; then
-              sed -i 's/^-l .*/-l 0.0.0.0/' /etc/memcached.conf
-            fi"
+# 2. Обновляем конфиг так, чтобы memcached слушал на 0.0.0.0
 run "$ALT" "if [ -f /etc/sysconfig/memcached ]; then
-              sed -i 's/^OPTIONS=.*/OPTIONS=\"-l 0.0.0.0 -m 64\"/' /etc/sysconfig/memcached
-            fi"
+               sed -i 's/^OPTIONS=.*/OPTIONS=\"-l 0.0.0.0 -m 64\"/' /etc/sysconfig/memcached; fi"
+run "$ALT" "if [ -f /etc/memcached.conf ]; then
+               sed -i 's/^-l .*/-l 0.0.0.0/' /etc/memcached.conf; fi"
 
-# 3. Перезапуск
-run "$ALT" "systemctl enable --now memcached"
+# 3. Перезапускаем сервис, чтобы подтянулся новый конфиг
+run "$ALT" "systemctl daemon-reload && systemctl enable --now memcached"
